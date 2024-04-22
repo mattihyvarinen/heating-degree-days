0aa7ace7f5fe170b0d056eeac3ae74a87b53eba6
Matti Hyvärinen <matti.hyvarinen@iki.fi>
2024-04-22 19:53:54 +0300

    Add graph component

---
+++ src/app/graph/graph.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graph',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      graph works!
+    </p>
+  `,
+  styleUrl: './graph.component.scss'
+})
+export class GraphComponent {
+
+}
