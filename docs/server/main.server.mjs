5b4d379b69921106d0508c310acf441836c754cf
Matti Hyvärinen <matti.hyvarinen@iki.fi>
2024-05-27 07:00:44 +0300

    Deploy to GitHub Page
    https://angular.io/guide/deployment

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-GEXTX2MW.mjs";import"./chunk-XGA63UKG.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-K4JRZAIB.mjs";import"./chunk-XGA63UKG.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
